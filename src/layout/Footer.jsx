94c1c49e696c916e334f1b0424f4f7899dbafd79
ZavalaGT <gzt-95@hotmail.com>
2022-11-13 09:49:42 -0600

    Add footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,46 @@
+import { Box, Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
     return (
         <footer className={"footer"}>
-                <Grid container>
-                    <Grid item >
-
+            <Container maxWidth={'md'}>
+                <Grid my={2} container justifyContent={'center'}>
+                    <Grid item xs={11} md={4} className={'vh-center'} sx={{justifyContent:{xs:'center', md:'left'}}}>
+                        <Typography variant={"caption"} component={'p'}>
+                            Aviso de privacidad / Contacto
+                        </Typography>
                     </Grid>
-
+                    <Grid item xs={11} md={4} className={'vh-center'}>
+                        <img src={'src/assets/img/logo-sochi.png'} alt={'logo-sochi'} className={'icon-logo'} />
+                    </Grid>
+                    <Grid item xs={11} md={4} className={'vh-center'}>
+                        <Grid container justifyContent={'center'}>
+                            <Grid item xs={2} md={2} className={'vh-center'}>
+                                <img src={'src/assets/img/mobli-icon.png'} alt={'logo-sochi'} className={'icon-logo mobil'} />
+                            </Grid>
+                            <Grid item xs={2} md={2} className={'vh-center'}>
+                                <Box className={'icon-logo twitter vh-center'}>
+                                    <TwitterIcon  />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={2} md={2} className={'vh-center'}>
+                                <Box className={'icon-logo facebook vh-center'}>
+                                    <FacebookIcon  />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={2} md={2} className={'vh-center'}>
+                                <Box className={'icon-logo youtube vh-center'}>
+                                    <YouTubeIcon  />
+                                </Box>                           
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
+            </Container>
         </footer>
     )
 }
